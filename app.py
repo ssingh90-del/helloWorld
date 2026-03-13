0d3a96f14c2fc475349c6300c1dfd3417ff7d977
ssingh90-del <ssingh90@terpmail.umd.edu>
2026-03-13 08:41:06 -0400

    Created a base parent template alongside two child templates that create a contact form and return the favorite course respectively

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -18,6 +17,19 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
